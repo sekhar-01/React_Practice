d2cbc1e9f4ba9b3f66d6270e9de5c8f643b85ea6
HAREESH MOLLETI <95898216+hareesh2504@users.noreply.github.com>
2022-02-17 17:17:04 +0530

    Create class.js

---
+++ class_components/class.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Class_Function extends React.Component {
+    render() {
+        return (
+        <div>
+            hareesh
+        </div >
+    );
+}
+}
+
+export default Class_Function;
