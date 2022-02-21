30465dd51b482fbb7850a4ce521aeee92a4b7304
HAREESH MOLLETI <95898216+hareesh2504@users.noreply.github.com>
2022-02-21 10:29:57 +0530

    Add files via upload

---
+++ form/form-display/Footer.js
@@ -0,0 +1,12 @@
+/* eslint-disable jsx-a11y/no-distracting-elements */
+import './Structure.css';
+
+function Footer() {
+    return(
+        <div className="footer">
+            <marquee direction="right"> Molleti Hareesh </marquee>
+        </div>
+    )
+}
+
+export default Footer;
